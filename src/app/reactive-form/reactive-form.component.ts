2af7a18cb4aad085a597057e0a42a31971d700c2
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-03 14:18:17 +0530

    TD Form Validation

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -7,22 +7,22 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent {
- contactForm = new FormGroup({
-  firstName : new FormControl('rahul'),
-  lastName : new FormControl(),
-  email : new FormControl(),
-  gender : new FormControl(),
-  isMarried : new FormControl(),
-  country : new FormControl(),
- address: new FormGroup({
-  city: new FormControl(),
-  street:new FormControl(),
-  pinCode:new FormControl()
+//  contactForm = new FormGroup({
+//   firstName : new FormControl('rahul'),
+//   lastName : new FormControl(),
+//   email : new FormControl(),
+//   gender : new FormControl(),
+//   isMarried : new FormControl(),
+//   country : new FormControl(),
+//  address: new FormGroup({
+//   city: new FormControl(),
+//   street:new FormControl(),
+//   pinCode:new FormControl()
 
- })
+//  })
 
- })
- onSubmit(){
-  console.log(this.contactForm.value);
- }
+//  })
+//  onSubmit(){
+//   console.log(this.contactForm.value);
+//  }
 }
