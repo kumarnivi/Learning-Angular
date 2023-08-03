f6a6c10281edceeb5391cd75ccd06ce26edcae90
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-03 11:27:52 +0530

    Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
 import { HeadingComponent } from './heading/heading.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component'
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +18,14 @@ import { TemplateDrivenFormComponent } from './template-driven-form/template-dri
     ListComponent,
     HeadingComponent,
     TemplateDrivenFormComponent,
+    ReactiveFormComponent,
   
   ],
   imports: [ //here use for 2 way binding
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent {
+ contactForm = new FormGroup({
+  firstName : new FormControl('rahul'),
+  lastName : new FormControl(),
+  email : new FormControl(),
+  gender : new FormControl(),
+  isMarried : new FormControl(),
+  country : new FormControl(),
+ address: new FormGroup({
+  city: new FormControl(),
+  street:new FormControl(),
+  pinCode:new FormControl()
+
+ })
+
+ })
+ onSubmit(){
+  console.log(this.contactForm.value);
+ }
+}
