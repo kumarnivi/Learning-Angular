43ea400e8890e4ce152155559c73ffd35348d359
kumarnivi <nivethikashivakumar56@gmail.com>
2023-12-05 17:31:40 +0530

    swiper slider

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -10,36 +10,47 @@ export class SwiperComponent {
 
   ngOnInit(): void {
     new Swiper('.clients-slider', {
+      effect:'coverflow',
+      grabCursor:true,
+      centeredSlides:true,
+      slidesPerView: 'auto',
+      coverflowEffect:{
+  rotate:50,
+  stretch:0,
+  depth:200,
+  modifier:1,
+  slideShadows:true
+      },
       speed: 400,
       loop: true,
       autoplay: {
-        delay:1000,
+        delay:2000,
         disableOnInteraction: false
       },
-      slidesPerView: 'auto',
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 40
-        },
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 60
-        },
-        640: {
-          slidesPerView: 4,
-          spaceBetween: 80
-        },
-        992: {
-          slidesPerView: 6,
-          spaceBetween: 120
-        }
-      }
+      // slidesPerView: 'auto',
+      // pagination: {
+      //   el: '.swiper-pagination',
+      //   type: 'bullets',
+      //   clickable: true
+      // },
+      // breakpoints: {
+      //   320: {
+      //     slidesPerView: 2,
+      //     spaceBetween: 40
+      //   },
+      //   480: {
+      //     slidesPerView: 3,
+      //     spaceBetween: 60
+      //   },
+      //   640: {
+      //     slidesPerView: 4,
+      //     spaceBetween: 80
+      //   },
+      //   992: {
+      //     slidesPerView: 6,
+      //     spaceBetween: 120
+      //   }
+      // }
     });
   }
 
